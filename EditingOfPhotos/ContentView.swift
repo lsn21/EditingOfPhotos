9404cf94a88023bc086d4993283c3c296c9cf8da
SIARHEI LUKYANAU <lsn21@ya.ru>
2024-09-07 15:28:52 +0300

    Initial commit

---
+++ EditingOfPhotos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EditingOfPhotos
+//
+//  Created by SIARHEI LUKYANAU on 07.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EditingOfPhotos/EditingOfPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditingOfPhotosApp.swift
+//  EditingOfPhotos
+//
+//  Created by SIARHEI LUKYANAU on 07.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EditingOfPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
